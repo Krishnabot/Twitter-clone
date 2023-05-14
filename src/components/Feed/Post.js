d66f358bf2e0b700a7f2b8bc3f1788608405da50
Krishna <libraryismyparadise@gmail.com>
2023-05-14 13:05:29 +0545

    Add Feed

---
+++ src/components/Feed/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './post.css'
+import { Verified } from '@mui/icons-material'
+
+function Post() {
+  return (
+    <div className="post">Post</div>
+  )
+}
+
+export default Post
